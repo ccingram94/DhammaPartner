7eeacf2d0a600b1ef4f06c5e846a22cfaf543c59
Constance Ingram <constanceingram94@gmail.com>
2018-05-05 18:45:15 -0500

    I honestly have no idea

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
+	passport = require("passport"),
+	LocalStrategy = require("passport-local"),
+	User = require("./models/user"),
+
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "If you see the Buddha in the road, kill him.",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.listen(3000);
+console.log("It's alive!!!!");
+
